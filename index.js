02fdf33ce059ea2e4c6dd6ff79e41b72b5da6484
0moose <79241242+0moose@users.noreply.github.com>
2023-03-07 15:16:14 +0100

    update

---
+++ index.js
@@ -0,0 +1,74 @@
+/*
+let mainbody = document.getElementById('main-body');
+let body = document.getElementById('body');
+let contactdetails = document.getElementById('contactdetails');
+let Fname = document.getElementById('Fname');
+let details = document.getElementById('details');
+let profile = document.getElementById('profile');
+let title = document.getElementById('title');
+let text1 = document.getElementById('text1').innerHTML;
+let text2 = document.getElementById('text3');
+let text3 = document.getElementById('text3');
+let text4 = document.getElementById('text4');
+let text5 = document.getElementById('text5');
+let text6 = document.getElementById('text6');
+let text7= document.getElementById('text7');
+let text8 = document.getElementById('text8');
+let work = document.getElementById('work');
+let education = document.getElementById('education');
+let training = document.getElementById('training');
+let volunteer = document.getElementById('volunteer');
+let other = document.getElementById('other');
+let skills = document.getElementById('skills');
+let referees = document.getElementById('referees');
+*/
+
+document.getElementById('Fname').innerHTML = "MOSES MORDI";
+
+
+document.getElementById('details').innerHTML += "Benin Edo  | mosesnmordi@gmail.com | github.com/0moose | 08145561089";
+
+document.getElementById('Ptitle').innerHTML = "PROFILE";
+
+document.getElementById('para1').innerHTML = "Started learning web development on my own during the Covid-19 shutdown in 2020. studied HTML, CSS, and JavaScript before switching to PHP and Python later on. Throughout my time at the workshop, I learned how to tackle real-world technical challenges and how to adapt to changes as we worked to solve them. I also gained experience in customer services and relationship development.";
+
+document.getElementById('Wtitle').innerHTML = "WORK EXPERIENCE";
+
+document.getElementById('hit').innerHTML = "Automobile Mechanic WorkShop, Kaura Namoda, Zamfara";
+
+document.getElementById('eat').innerHTML = "June 2020 - December 2020";
+
+document.getElementById('list').innerHTML += "<p><strong>Position</strong> : Apperentice </br> <ul><li>Repaired Cars dashboard malfunction</li> <li>Electrical wiring maintainace";
+
+document.getElementById('Etitle').innerHTML = "EDUCATION";
+
+document.getElementById('edu').innerHTML = "B.Eng, Structural Engineering";
+
+document.getElementById('cation').innerHTML = "2018 - 2023";
+
+document.getElementById('school').innerHTML = "University of Benin, Nigeria";
+
+document.getElementById('Ttitle').innerHTML = "CERTIFICATION";
+
+document.getElementById('cert').innerHTML = "Jobberman Soft Skills Training Certification";
+
+document.getElementById('timing').innerHTML = "2022";
+
+document.getElementById('Stitle').innerHTML = "SKILLS";
+
+document.getElementById('interests').innerHTML += "<ul><li>PHP</li> <li>JavaScript</li> <li>Python</li>";
+
+document.getElementById('Rtitle').innerHTML = "REFEREES";
+
+document.getElementById('help').innerHTML = "Available on request";
+
+
+
+
+
+
+
+
+
+
+
