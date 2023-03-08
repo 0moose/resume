428a058ad883c6639c5b64efeb101a1bd3e57556
0moose <79241242+0moose@users.noreply.github.com>
2023-03-08 21:14:37 +0100

    update

--- frontend/index.js
+++ frontend/index.js
@@ -82,10 +82,10 @@ function github() {
     let link = document.createTextNode("Github");
 
     //append the text node to anchor element.
-    a.appendChild(link);
+   // a.appendChild(link);
 
     //set the title
-    a.title = "Github";
+    //a.title = "Github";
 
     //set the href property
     a.href = "https://github.com/0moose";
